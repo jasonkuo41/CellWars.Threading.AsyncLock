e258273ab41acdcb7bef56c5703eed6b65d4811b
Jason Kuo <jason.kuo41@gmail.com>
2019-07-27 19:14:10 +0800

    Update AsyncLock API

--- src/CellWars.Async/AsyncLock.cs
+++ src/CellWars.Async/AsyncLock.cs
@@ -192,7 +192,7 @@ namespace CellWars.Threading {
         /// <param name="span">The timeout to this lock, default value null would fallback using DefaultTimeOut</param>
         /// <param name="ct">The CancellationToken to cancel waiting</param>
         /// <returns>The field's value</returns>
-        public TValue AcquireField<TValue>(Func<T, TValue> field, TimeSpan? span, CancellationToken ct = default) {
+        public TValue AcquireField<TValue>(Func<T, TValue> field, TimeSpan? span, CancellationToken ct) {
             using (Lock(span, ct))
                 return field(_holder);
         }
@@ -212,7 +212,7 @@ namespace CellWars.Threading {
         /// <param name="field">The acquiring field</param>
         /// <param name="value">The operation on to the field</param>
         /// <param name="span">The timeout to this lock, default value null would fallback using DefaultTimeOut</param>
-        public void SetField<TValue>(Func<T, TValue> field, Action<TValue> value, TimeSpan? span) => SetField(field, value, span);
+        public void SetField<TValue>(Func<T, TValue> field, Action<TValue> value, TimeSpan? span) => SetField(field, value, span, default);
 
         /// <summary>
         /// A helper function to write or set a field within a class with thread-safety concerned
@@ -230,7 +230,7 @@ namespace CellWars.Threading {
         /// <param name="value">The operation on to the field</param>
         /// <param name="span">The timeout to this lock, default value null would fallback using DefaultTimeOut</param>
         /// <param name="ct">The CancellationToken to cancel waiting</param>
-        public void SetField<TValue>(Func<T, TValue> field, Action<TValue> value, TimeSpan? span, CancellationToken ct = default) {
+        public void SetField<TValue>(Func<T, TValue> field, Action<TValue> value, TimeSpan? span, CancellationToken ct) {
             using (Lock(span, ct))
                 value(field(_holder));
         }
