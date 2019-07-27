8f7e930836971deb54fb20acf2ae285cbd8a9c73
Jason Kuo <jason.kuo41@gmail.com>
2019-07-27 19:00:58 +0800

    Update AsyncLock Public API and Update Travis

--- src/CellWars.Async/AsyncLock.cs
+++ src/CellWars.Async/AsyncLock.cs
@@ -164,6 +164,23 @@ namespace CellWars.Threading {
         /// <summary>
         /// A helper function to acquire a field within the class with thread-safety concerned
         /// </summary>
+        /// <typeparam name="TValue">The fields type</typeparam>
+        /// <param name="field">The acquiring field</param>
+        /// <returns>The field's value</returns>
+        public TValue AcquireField<TValue>(Func<T, TValue> field) => AcquireField(field, null, default);
+
+        /// <summary>
+        /// A helper function to acquire a field within the class with thread-safety concerned
+        /// </summary>
+        /// <typeparam name="TValue">The fields type</typeparam>
+        /// <param name="field">The acquiring field</param>
+        /// <param name="ct">The CancellationToken to cancel waiting</param>
+        /// <returns>The field's value</returns>
+        public TValue AcquireField<TValue>(Func<T, TValue> field, CancellationToken ct) => AcquireField(field, null, ct);
+
+        /// <summary>
+        /// A helper function to acquire a field within the class with thread-safety concerned
+        /// </summary>
         /// <example>
         /// This is commonly used for tidier get property
         /// <code>
@@ -175,11 +192,27 @@ namespace CellWars.Threading {
         /// <param name="span">The timeout to this lock, default value null would fallback using DefaultTimeOut</param>
         /// <param name="ct">The CancellationToken to cancel waiting</param>
         /// <returns>The field's value</returns>
-        public TValue AcquireField<TValue>(Func<T, TValue> field, TimeSpan? span = null, CancellationToken ct = default) {
+        public TValue AcquireField<TValue>(Func<T, TValue> field, TimeSpan? span, CancellationToken ct = default) {
             using (Lock(span, ct))
                 return field(_holder);
         }
 
+        /// <summary>
+        /// A helper function to write or set a field within a class with thread-safety concerned
+        /// </summary>
+        /// <typeparam name="TValue">The fields type</typeparam>
+        /// <param name="field">The acquiring field</param>
+        /// <param name="value">The operation on to the field</param>
+        public void SetField<TValue>(Func<T, TValue> field, Action<TValue> value) => SetField(field, value, null);
+
+        /// <summary>
+        /// A helper function to write or set a field within a class with thread-safety concerned
+        /// </summary>
+        /// <typeparam name="TValue">The fields type</typeparam>
+        /// <param name="field">The acquiring field</param>
+        /// <param name="value">The operation on to the field</param>
+        /// <param name="span">The timeout to this lock, default value null would fallback using DefaultTimeOut</param>
+        public void SetField<TValue>(Func<T, TValue> field, Action<TValue> value, TimeSpan? span) => SetField(field, value, span);
 
         /// <summary>
         /// A helper function to write or set a field within a class with thread-safety concerned
@@ -197,8 +230,7 @@ namespace CellWars.Threading {
         /// <param name="value">The operation on to the field</param>
         /// <param name="span">The timeout to this lock, default value null would fallback using DefaultTimeOut</param>
         /// <param name="ct">The CancellationToken to cancel waiting</param>
-        /// <returns>The field's value</returns>
-        public void SetField<TValue>(Func<T, TValue> field, Action<TValue> value, TimeSpan? span = null, CancellationToken ct = default) {
+        public void SetField<TValue>(Func<T, TValue> field, Action<TValue> value, TimeSpan? span, CancellationToken ct = default) {
             using (Lock(span, ct))
                 value(field(_holder));
         }
